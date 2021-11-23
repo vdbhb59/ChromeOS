40a71277e88ed9439777949d5a0bbffe29a98ede
vdbhb59 <60728004+vdbhb59@users.noreply.github.com>
2021-11-23 11:46:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get install pv
 sudo apt-get install cgpt
-sudo bash chromeos-install.sh -src zork_recovery.bin -dst /dev/sda
+sudo bash chromeos-install.sh -src zork_recovery.bin -dst /dev/sdb
